e550c5d31b397871be8f4f1f27d0b924dab78fb2
Andrey Komendantov <9400878@gmail.com>
2021-09-28 17:36:05 +0300

    feat: added reverse for absolute url

--- shop/models.py
+++ shop/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -12,6 +13,9 @@ class Category(models.Model):
 
         def __str__(self):
             return self.verbose_name
+
+    def get_absolut_url(self):
+        return reverse('shop:product_list_by_category', args=[self.slug])
 
 
 class Product(models.Model):
@@ -34,4 +38,7 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def get_absolut_url(self):
+        return reverse('shop:product_details', args=[self.id, self.slug])
 

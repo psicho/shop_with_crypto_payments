1fce7e42d71cc2a46d70a049116d1d081b3c2cac
Andrey Komendantov <9400878@gmail.com>
2021-09-28 18:17:53 +0300

    feat: added views for shop product list

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from .models import Category, Product
 
-# Create your views here.
+
+class ProductListView(ListView):
+    template_name = "shop/product/product_list.html"
+
+    def get_context_data(self, **kwargs):
+        categories = Category.objects.all()
+        product = Product.objects.filter(available=True)
+        category_slug = kwargs.get('category_slug')
+
+        if category_slug:
+            pass
+
+        context = {
+            'product': product,
+            'category': categories
+        }
+        return context
+
+    def get_queryset(self):
+        return Product.objects.all()

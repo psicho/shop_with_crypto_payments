ae454565e8e5f95bdc89fa0c914240ec12c97cf2
Andrey Komendantov <9400878@gmail.com>
2021-10-06 12:49:40 +0300

    feat: updated cart and orders

--- orders/models.py
+++ orders/models.py
@@ -1,17 +1,20 @@
 from django.db import models
 from shop.models import Product
+from django.utils.translation import ugettext_lazy as _
 
 
 class Order(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
+    first_name = models.CharField(verbose_name=_('Имя'), max_length=50)
+    last_name = models.CharField(verbose_name=_('Фамилия'), max_length=50)
     email = models.EmailField()
-    address = models.CharField(max_length=250)
-    postal_code = models.CharField(max_length=20)
-    city = models.CharField(max_length=100)
+    address = models.CharField(verbose_name=_('Адрес'), max_length=250)
+    postal_code = models.CharField(verbose_name=_('Индекс'), max_length=20)
+    city = models.CharField(verbose_name=_('Город'), max_length=100)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     paid = models.BooleanField(default=False)
+    ethereum_wallet = models.CharField(verbose_name=_('Ethereum wallet (кошелёк с которого '
+                                                      'будет производиться оплата заказа)'), max_length=50)
 
     class Meta:
         ordering = ('-created',)
